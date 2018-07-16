f553a3c08d7faa0d0f869f785f944fec2fb2ee0e
Milko Yanakiev <myanakiev@hotmail.com>
2018-07-16 17:32:05 +0300

    add

--- src/IteratorsComparators/StrategyPattern/Main.java
+++ src/IteratorsComparators/StrategyPattern/Main.java
@@ -3,21 +3,24 @@ package IteratorsComparators.StrategyPattern;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        List<Person> people = new ArrayList<>();
 
         int n = Integer.parseInt(reader.readLine());
 
         for (int i = 0; i < n; i++) {
             String[] token = reader.readLine().split("\\s+");
             Person person = new Person(token[0], Integer.parseInt(token[1]));
-
-
-
+            people.add(person);
         }
+
+
+
 
 
 
