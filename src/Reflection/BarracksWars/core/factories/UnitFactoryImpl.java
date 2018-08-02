70f347e44364004b040e9c9493e11253af26b35b
Milko Yanakiev <myanakiev@hotmail.com>
2018-08-02 18:50:41 +0300

    add new now

--- src/Reflection/BarracksWars/core/factories/UnitFactoryImpl.java
+++ src/Reflection/BarracksWars/core/factories/UnitFactoryImpl.java
@@ -4,6 +4,9 @@ import Reflection.BarracksWars.contracts.Unit;
 import Reflection.BarracksWars.contracts.UnitFactory;
 import Reflection.BarracksWars.models.units.Swordsman;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class UnitFactoryImpl implements UnitFactory {
 
     private static final String UNITS_PACKAGE_NAME = "Reflection.BarracksWars.models.units.";
@@ -11,17 +14,36 @@ public class UnitFactoryImpl implements UnitFactory {
     @Override
     public Unit createUnit(String unitType) {
 
-
         Unit unit = null;
+
+        try {
+            Class<?> unitClass = Class.forName(UNITS_PACKAGE_NAME + unitType);
+            Constructor<?> declaredConstructor = unitClass.getDeclaredConstructor();
+            unit = (Unit) declaredConstructor.newInstance();
+
+
+
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+        /*
         switch (unitType){
             case "Swordsman":
-                unit = new Swordsman();
-                break;
+                return new Swordsman();
         }
+        return null;
+        */
+
+
         return unit;
 
-
-
-        // TODO: implement for problem 3
     }
 }
